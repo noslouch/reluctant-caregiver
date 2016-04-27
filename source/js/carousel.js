f51c888a2c8770fcdf2ea0f4dae070bd4f9fd863
Brian Whitton <me@brianwhitton.com>
2016-04-26 23:00:16 -0400

    adds sliders to homepage

---
+++ source/js/carousel.js
@@ -0,0 +1,10 @@
+$(function() {
+  $('.slider').slick({
+    dots: true,
+    slide: 'article.thumbnail',
+    slidesToShow: 3,
+    speed: 150,
+    nextArrow: '<button class="btn btn--next" type="button"><i class="fa fa-3x fa-long-arrow-right" aria-hidden="true"></i></button>',
+    prevArrow: '<button class="btn btn--prev" type="button"><i class="fa fa-3x fa-long-arrow-left" aria-hidden="true"></i></button>'
+  });
+});
