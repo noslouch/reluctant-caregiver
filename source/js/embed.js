f26a78a06b626e0d4691daf71d30a3d682608d4d
Brian Whitton <me@brianwhitton.com>
2016-04-28 01:13:38 -0400

    adds youtube iframe embed

---
+++ source/js/embed.js
@@ -0,0 +1,38 @@
+(function() {
+  var mainImage = document.querySelector('#mainImage');
+  var imageWrapper = mainImage.parentElement;
+  var url = processURL(window.CAREGIVER && window.CAREGIVER.ytUrl);
+  if (!mainImage || !url) {
+    return;
+  }
+
+  function embedVideo(e) {
+    var currentHeight = imageWrapper.clientHeight;
+    var iframe = generateIframe(currentHeight, url);
+    imageWrapper.removeChild(mainImage);
+    imageWrapper.appendChild(iframe);
+    imageWrapper.classList.add('is-video');
+  }
+
+  function generateIframe(height, url) {
+    var iframe = document.createElement('iframe');
+    iframe.setAttribute('src', url);
+    iframe.setAttribute('height', height);
+    iframe.setAttribute('width', '100%');
+    iframe.setAttribute('frameborder', 0);
+    iframe.setAttribute('webkitallowfullscreen', true);
+    iframe.setAttribute('mozallowfullscreen', true);
+    iframe.setAttribute('allowfullscreen', true);
+    return iframe;
+  }
+
+  function processURL(fullUrl) {
+    if (!fullUrl) {
+      return false;
+    }
+    var parts = fullUrl.split('=');
+    return `https:\/\/www.youtube.com\/embed\/${parts[1]}`;
+  }
+
+  imageWrapper.addEventListener('click', embedVideo, false);
+})();
