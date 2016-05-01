6aecbc17585e67b41392b014975478eb23df3c2b
Brian Whitton <me@brianwhitton.com>
2016-05-01 15:46:57 -0400

    set header threshold to a constant, makes for better scroll transition

--- source/js/scroll.js
+++ source/js/scroll.js
@@ -2,13 +2,13 @@
 'use strict';
 
 $(function() {
+  const headerThreashold = 162;
   let content = document.querySelector('#content');
   let header = document.querySelector('#header');
 
   function measureContent() {
     let { top: contentTop } = content.getBoundingClientRect();
-    let { bottom: headerBottom } = header.getBoundingClientRect();
-    if (contentTop < headerBottom) {
+    if (contentTop < headerThreashold) {
       header.classList.add('siteheader--small');
     } else {
       header.classList.remove('siteheader--small');
